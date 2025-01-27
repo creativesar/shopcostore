926fa480989954692bf2519a20bbd3f62ce29b5a
uniqueluck68@gmail.com <your_email@example.com>
2025-01-28 04:46:39 +0500

    ok

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,37 +1,37 @@
-// import { client } from "@/sanity/lib/client";
-// import { NextRequest, NextResponse } from "next/server";
+import { client } from "@/sanity/lib/client";
+import { NextRequest, NextResponse } from "next/server";
 
-// export async function GET(request: NextRequest) {
-//     try {
-//         const data = await client.fetch(`
-//             *[_type=="products"]{
-//                 _id,
-//                 name,
-//                 description,
-//                 price,
-//                 "imageUrl" : image.asset->url,
-//                 category,
-//                 discountPercent,
-//                 "isNew": new,
-//                 colors,
-//                 sizes
-//             }
-//         `);
-//         return NextResponse.json(data);
-//     } catch (error) {
-//         return NextResponse.json({ error: 'Failed to fetch products' }, { status: 500 });
-//     }
-// }
+export async function GET(request: NextRequest) {
+    try {
+        const data = await client.fetch(`
+            *[_type=="products"]{
+                _id,
+                name,
+                description,
+                price,
+                "imageUrl" : image.asset->url,
+                category,
+                discountPercent,
+                "isNew": new,
+                colors,
+                sizes
+            }
+        `);
+        return NextResponse.json(data);
+    } catch (error) {
+        return NextResponse.json({ error: 'Failed to fetch products' }, { status: 500 });
+    }
+}
 
-// export async function POST(request: NextRequest) {
-//     try {
-//         const body = await request.json();
-//         const result = await client.create({
-//             _type: 'products',
-//             ...body
-//         });
-//         return NextResponse.json(result, { status: 201 });
-//     } catch (error) {
-//         return NextResponse.json({ error: 'Failed to create product' }, { status: 500 });
-//     }
-// }
+export async function POST(request: NextRequest) {
+    try {
+        const body = await request.json();
+        const result = await client.create({
+            _type: 'products',
+            ...body
+        });
+        return NextResponse.json(result, { status: 201 });
+    } catch (error) {
+        return NextResponse.json({ error: 'Failed to create product' }, { status: 500 });
+    }
+}
