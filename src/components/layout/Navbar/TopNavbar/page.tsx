c86a79d3cfe4450819b91b1806d74f96aa038edd
uniqueluck68@gmail.com <your_email@example.com>
2025-02-02 01:03:07 +0500

    ok

--- src/components/layout/Navbar/TopNavbar/page.tsx
+++ src/components/layout/Navbar/TopNavbar/page.tsx
@@ -13,6 +13,7 @@ import Image from "next/image";
 import InputGroup from "@/components/ui/input-group";
 import ResTopNavbar from "./ResTopNavbar";
 import CartBtn from "./CartBtn";
+import UserIconLink from "@/components/Userbutton/page";
 
 const data: NavMenu = [
   {
@@ -132,16 +133,7 @@ const TopNavbar = () => {
             />
           </Link>
           <CartBtn />
-          <Link href="/sign-in" className="p-1">
-            <Image
-              priority
-              src="/icons/user.svg"
-              height={100}
-              width={100}
-              alt="user"
-              className="max-w-[22px] max-h-[22px]"
-            />
-          </Link>
+          <UserIconLink />
         </div>
       </div>
     </nav>
